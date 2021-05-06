d03190ed83527bafe84094f75ae5402e9c9d5c5f
Madan-444 <mmadan3600@gmail.com>
2021-05-06 12:22:00 +0545

    hamberger and home backgrouond finished

--- my-motion/src/components/HomePage.js
+++ my-motion/src/components/HomePage.js
@@ -7,10 +7,34 @@ function HomePage() {
         <div>
             {/* <cavas id="canvas2"></cavas> */}
            <div className="homepage-container">
-               <section className="homepage-intro"></section>
-               <section className="homepage-project"></section>
-               <section className="homepage-skills"></section>
-               <section className="homepage-about"></section>
+               {/* ********************** My intro Section ********************************************8 */}
+               <section id="homepage-intro">
+                   <nav className="header__main-nav">
+                       <input type='checkbox' />
+                       <div className="header__main-nav--hamburger"><div></div></div>
+                       <div className="header__main-nav--menu">
+                           <div>
+                               <div>
+                                   <ul>
+                                       <li><a href="#">Intro</a></li>
+                                       <li><a href="#">Projects</a></li>
+                                       <li><a href="#">Skills</a></li>
+                                       <li><a href="#">Resume</a></li>
+                                       <li><a href="#">My Notes</a></li>
+                                   </ul>
+                               </div>
+                           </div>
+                       </div>
+                   </nav>
+               </section>
+                 {/* ********************** My project showcase Section ********************************************8 */}
+               <section id="homepage-projects">Projects</section>
+                 {/* ********************** My skills Section ********************************************8 */}
+               <section id="homepage-skills">Skills</section>
+                 {/* ********************** My resume Section ********************************************8 */}
+               <section id="homepage-resume">Resume</section>
+                {/* ********************** My resume Section ********************************************8 */}
+               <section id="homepage-notes">My Notes</section>
 
            
            </div>
